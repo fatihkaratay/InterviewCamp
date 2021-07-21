327b3c0677610952442a2a34b3b6dcf36134ae5c
Fatih Karatay <fatih.karatay@ngc.com>
2021-07-21 17:08:33 -0400

    problem01 is coded

--- src/reversetraverse/Problem01.java
+++ src/reversetraverse/Problem01.java
@@ -1,7 +1,20 @@
 package reversetraverse;
 
+import java.util.Arrays;
+
 public class Problem01 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {1, 2, 0, 3, 4, 0};
+        System.out.println(Arrays.toString(arr));
+        duplicateEven(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void duplicateEven(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == 0) {
+                arr[i] = arr[i-1];
+            }
+        }
     }
 }
