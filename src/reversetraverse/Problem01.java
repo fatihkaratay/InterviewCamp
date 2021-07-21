802a61296290c1c3e019a3b1e4b1e504ef6a865a
Fatih Karatay <fatihkaratay@gmail.com>
2021-07-21 17:13:15 -0400

    test

--- src/reversetraverse/Problem01.java
+++ src/reversetraverse/Problem01.java
@@ -11,10 +11,8 @@ public class Problem01 {
     }
 
     private static void duplicateEven(int[] arr) {
-        for (int i = arr.length - 1; i >= 0; i--) {
-            if (arr[i] == 0) {
+        for (int i = arr.length - 1; i >= 0; i--)
+            if (arr[i] == 0)
                 arr[i] = arr[i-1];
-            }
-        }
     }
 }
