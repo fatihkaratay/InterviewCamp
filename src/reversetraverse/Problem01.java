226dd679a86ef1ff45b0aa28e212074fd135373c
Fatih Karatay <fatihkaratay@gmail.com>
2021-07-21 17:14:17 -0400

    method name is changed

--- src/reversetraverse/Problem01.java
+++ src/reversetraverse/Problem01.java
@@ -6,11 +6,11 @@ public class Problem01 {
     public static void main(String[] args) {
         int[] arr = {1, 2, 0, 3, 4, 0};
         System.out.println(Arrays.toString(arr));
-        duplicateEven(arr);
+        duplicateEvenNumbers(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void duplicateEven(int[] arr) {
+    private static void duplicateEvenNumbers(int[] arr) {
         for (int i = arr.length - 1; i >= 0; i--)
             if (arr[i] == 0)
                 arr[i] = arr[i-1];
