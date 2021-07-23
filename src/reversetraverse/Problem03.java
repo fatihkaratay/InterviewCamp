b0cb3addc00d380d48bfe110f243048813b9825b
Fatih Karatay <fatihkaratay@gmail.com>
2021-07-23 11:37:14 -0400

    order square method is implemented

---
+++ src/reversetraverse/Problem03.java
@@ -0,0 +1,41 @@
+package reversetraverse;
+
+import java.util.Arrays;
+
+public class Problem03 {
+    public static void main(String[] args) {
+        int[] arr = {-4,-2,-1,0,3,5};
+        int[] newArr = orderOfSquares(arr);
+        System.out.println(Arrays.toString(newArr));
+    }
+
+    public static int[] orderOfSquares(int[] arr) {
+        if (arr == null)
+            return null;
+        int start = 0, end = arr.length - 1;
+        int[] result = new int[arr.length];
+        int index = result.length - 1;
+
+        while (start <= end) {
+            int a = arr[start] * arr[start];
+            int b = arr[end] * arr[end];
+
+            if (a < b) {
+                result[index] = b;
+                end--;
+            } else {
+                result[index] = a;
+                start++;
+            }
+            index--;
+        }
+
+        return result;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
