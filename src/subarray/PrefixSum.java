3987142bd3def26a96a0d4187401a1edc8a9b4ee
Fatih Karatay <fatihkaratay@gmail.com>
2021-09-16 15:57:48 -0400

    prefix sum: short way

--- src/subarray/PrefixSum.java
+++ src/subarray/PrefixSum.java
@@ -15,79 +15,25 @@ public class PrefixSum {
     public static void main(String[] args) {
         int[] arr1 = {2,4,-2,1,-3,5,-3};
         int[] arr = {-1, 2, 1, -4, 2, 3, -1, 2};
+        System.out.println(Arrays.toString(prefixSum(arr1)));
         System.out.println(Arrays.toString(prefixSum(arr)));
     }
 
     public static int[] prefixSum(int[] arr) {
         if (arr == null || arr.length == 0) throw new IllegalArgumentException();
-        int sum = 0;
-        int[] sumArray = new int[arr.length];
 
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
-            sumArray[i] = sum;
-        }
-
-        int isZeroIncluded = isZeroIncluded(sumArray); // -1 or index
-        if (isZeroIncluded != -1) {
-            return new int[]{ 0, isZeroIncluded};
-        }
-
-        int[] isDuplicateIncluded = isDuplicateIncluded(sumArray);
-        if (isDuplicateIncluded[0] != -1 && isDuplicateIncluded[1] != -1) {
-            return new int[] {isDuplicateIncluded[0] + 1, isDuplicateIncluded[1]};
-        }
-
-        System.out.println(Arrays.toString(sumArray));
-        return null;
-    }
-
-    private static int isZeroIncluded(int[] arr) {
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i] == 0) {
-                return i;
+            if (sum == 0) return new int[] {0, i};
+            if (map.containsKey(sum)) {
+                return new int[] { map.get(sum) + 1, i};
             }
-        }
-        return -1;
-    }
-
-    @SuppressWarnings("Java8MapApi")
-    private static int[] isDuplicateIncluded(int[] arr) {
-        int[] result = new int[2];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int element : arr) {
-            if(map.get(element) == null) {
-                map.put(element, 1);
-            } else {
-                map.put(element, map.get(element) + 1);
-            }
+            map.put(sum, i);
         }
 
-        final int[] index = {0};
-        map.forEach((k,v) -> {
-            if (v == 2) {
-                index[0] = k;
-            }
-        });
-
-        int[] dupIndex = new int[2];
-        int i = 0, k = 0;
-        while (i < arr.length) {
-            if (arr[i] == index[0]) {
-                dupIndex[k++] = i;
-            }
-            i++;
-        }
-//        for (int i = 0; i < arr.length; i++) {
-//            if (arr[i] == index[0]) {
-//                for (int j = 0; j < 2; j++) {
-//                    dupIndex[j] = i;
-//                }
-//            }
-//        }
-
-        return dupIndex;
-
+        return new int[] {};
     }
 
 
