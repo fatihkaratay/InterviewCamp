162c0b55c4dbe4f1b458754960cc202a28765e1d
Fatih Karatay <fatihkaratay@gmail.com>
2021-09-28 10:49:47 -0400

    binary search with duplicate elements implementation

---
+++ src/binary_search/BinarySearchDuplicates.java
@@ -0,0 +1,33 @@
+package binary_search;
+
+/*
+(Level: Easy) Given a sorted array that can contain duplicates,
+find the first occurrence of a target element T.
+For example, if A = [2,3,4,4,5,6] and T = 4, return index 2.
+*/
+
+public class BinarySearchDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 4, 4, 5, 7, 8, 11};
+        int target = 4;
+
+        System.out.println(binarySearch(arr, target));
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid] || target == arr[mid] && mid >0 && arr[mid-1] == target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
