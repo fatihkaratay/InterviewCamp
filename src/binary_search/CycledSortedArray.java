0050c624b7b2e2801b0a10258d641bfec638abef
Fatih Karatay <fatihkaratay@gmail.com>
2021-09-30 11:19:23 -0400

    get min in cycled sortaed array

---
+++ src/binary_search/CycledSortedArray.java
@@ -0,0 +1,34 @@
+package binary_search;
+
+/*
+Given an array that is cyclically sorted, find the minimum element.
+A cyclically sortedarray is a sorted array rotated by some number of elements.
+Assume all elementsare unique.
+For example:A = [4,5,1,2,3], which is just [1,2,3,4,5] rotated by 2Result = index 2
+ */
+
+public class CycledSortedArray {
+    public static void main(String[] args) {
+        int[] arr = {4,5,1,2,3};
+        System.out.println(getMinCycledArray(arr));
+    }
+
+    public static int getMinCycledArray(int[] arr) {
+        if (arr == null) return -1;
+
+        int start = 0, end = arr.length - 1, right = arr[arr.length - 1];
+
+        while (start <= end) {
+            int mid = start + ((end - start) >> 1);
+            if (arr[mid] <= right && (mid == 0 || arr[mid-1] > arr[mid])) {
+                return mid;
+            } else if (arr[mid] > right) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
