54e24ac1312f4baa74304161561372b16136853e
Fatih Karatay <fatihkaratay@gmail.com>
2021-10-19 11:54:21 -0400

    Homework problems are added

---
+++ src/C06_recursion_backtracking/Homework03.java
@@ -0,0 +1,7 @@
+package C06_recursion_backtracking;
+/*
+Level: MediumSudokuSolver:​ Given a Sudoku board, find a solution. The board can have some squares filledout already. You have to fill the rest of the squares.(Rules of Sudoku are as follows: In each column, row and 3 x 3 square, you cannothave duplicate numbers. Also, only numbers 1-9 are allowed.)
+ */
+
+public class Homework03 {
+}
---
+++ src/C06_recursion_backtracking/Homework04.java
@@ -0,0 +1,21 @@
+package C06_recursion_backtracking;
+/*
+Level: Medium
+Word Break Problem : Given a String S, which contains letters and no spaces,
+determine if you
+can break it into valid words. Return one such combination of words.
+You can assume that you are provided a dictionary of English words.
+
+For example:
+S = "ilikemangotango"
+
+Output:
+Return any one of these:
+"i like mango tango"
+"i like man go tan go"
+"i like mango tan go"
+"i like man go tango"
+ */
+
+public class Homework04 {
+}
