0d6123cc06c97d9d524295638ad303463a90c0fb
Fatih Karatay <fatihkaratay@gmail.com>
2021-10-20 15:19:38 -0400

    Pair class i simplemented

--- src/C00_utils/Pair.java
+++ src/C00_utils/Pair.java
@@ -1,25 +1,19 @@
 package C00_utils;
 
 public class Pair {
-    private int a;
-    private int b;
+    private int i;
+    private int j;
 
-    private int[] points;
-
-    public Pair(int x, int y) {
-        this.a = x;
-        this.b = y;
-
-        this.points = new int[2];
-        this.points[0] = x;
-        this.points[1] = y;
+    public Pair(int i, int j) {
+        this.i = i;
+        this.j = j;
     }
 
-    public int getFirst() {
-        return this.points[0];
+    public int i() {
+        return i;
     }
 
-    public int getSecond() {
-        return this.points[1];
+    public int j() {
+        return j;
     }
 }
--- src/C06_recursion_backtracking/Homework03.java
+++ src/C06_recursion_backtracking/Homework03.java
@@ -1,6 +1,10 @@
 package C06_recursion_backtracking;
 /*
-Level: MediumSudokuSolver:​ Given a Sudoku board, find a solution. The board can have some squares filledout already. You have to fill the rest of the squares.(Rules of Sudoku are as follows: In each column, row and 3 x 3 square, you cannothave duplicate numbers. Also, only numbers 1-9 are allowed.)
+Level: MediumSudokuSolver:​ Given a Sudoku board, find a solution.
+The board can have some squares filled out already.
+You have to fill the rest of the squares.
+(Rules of Sudoku are as follows: In each column, row and 3 x 3 square,
+you cannot have duplicate numbers. Also, only numbers 1-9 are allowed.)
  */
 
 public class Homework03 {
