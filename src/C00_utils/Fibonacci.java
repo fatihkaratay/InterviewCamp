ae386c6af72ed44b664679e118e511b2b50fa82e
Fatih Karatay <fatihkaratay@gmail.com>
2022-01-17 15:35:38 -0500

    fibonacci memoization (DP) way is added

--- src/C00_utils/Fibonacci.java
+++ src/C00_utils/Fibonacci.java
@@ -5,10 +5,28 @@ public class Fibonacci {
         int n = 6;
 
         System.out.println(fibRec(n));
+        System.out.println(fibMemo(n));
     }
 
     public static int fibRec(int n) {
         if (n == 1 || n == 2) return 1;
         return fibRec(n - 1) + fibRec(n - 2);
+    }
+
+    public static int fibMemo(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int[] dp = new int[n]; // [1, 1, 2, 3, 5, 8];
+        if (n == 1 || n == 2) return 1;
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i-1] + dp[i - 2];
+        }
+
+        return dp[n - 1];
     }
 }
